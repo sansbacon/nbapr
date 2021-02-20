4e316546562dc2cca475fc54ceb2088b51f46d75
sansbacon <eric@erictruett.com>
2021-02-20 11:46:22 -0600

    Updated docs to include player rater app

---
+++ docs/player-rater-8cat.md
@@ -0,0 +1,48 @@
+<script type="text/javascript" charset="utf8" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.js"></script>
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.css">
+
+<script type="text/javascript">
+var url = "https://raw.githubusercontent.com/sansbacon/nbapr/main/data/player-rater-8cat.json";
+$(document).ready(function() {
+  var oTable = $('#pr').DataTable( {
+    "ajax": url,
+    "iDisplayLength": 250,
+    "dom": '<"pull-left"f><"pull-right"l>t',
+    "order": [1, 'desc'],
+    "columnDefs": [
+        {"className": "dt-center", "targets": "_all"}
+      ]
+    } );
+
+  oTable.$('th').tooltip( {
+      "delay": 0,
+      "track": true,
+      "fade": 250
+  } );
+
+  $.fn.dataTable.ext.errMode = 'none';
+  $('#pr')
+    .on( 'error.dt', function ( e, settings, techNote, message ) {
+    console.log( 'An error has been reported by DataTables: ', message );
+    alert('Request failed: please use valid values');
+  }).DataTable();
+});
+</script>
+
+# NBA Player Rater: 8 Category
+
+<table id="pr" class="display">
+    <thead>
+        <tr>
+            <th>Player</th>
+            <th>Points</th>
+        </tr>
+    </thead>
+    <tfoot>
+        <tr>
+            <th>Player</th>
+            <th>Points</th>
+        </tr>
+    </tfoot>
+</table>
--- scripts/update_datafiles.py
+++ scripts/update_datafiles.py
@@ -3,7 +3,9 @@
 # Copyright (C) 2021 Eric Truett
 # Licensed under the MIT License
 
+import json
 from pathlib import Path
+
 from nbapr import sim
 from nbapr.stats import get_stats
 
@@ -11,18 +13,28 @@ from nbapr.stats import get_stats
 def run():
     """Runs update script"""
     
+    mapping = {
+      '8cat': ['WFGP', 'WFTP', 'FG3M', 'REB', 'AST', 'STL', 'BLK', 'PTS'],
+      '9cat': ['WFGP', 'WFTP', 'FG3M', 'REB', 'AST', 'STL', 'BLK', 'PTS', 'TOV'],
+      '9catftm': ['WFGP', 'FTM', 'FG3M', 'REB', 'AST', 'STL', 'BLK', 'PTS', 'TOV']
+    }
+
     pool = get_stats(season='20-21')        
 
-    # run sim with specified parameters
-    results = sim(
-        pool=pool, 
-        n_iterations=15000, 
-        n_teams=10, 
-        n_players=12
-    )
+    # NEED TO RUN SIM 3 TIMES
+    for catname, catstats in mapping.items():
+        results = sim(
+            pool=pool,
+            n_iterations=1000, 
+            n_teams=10, 
+            n_players=12,
+            statscols=catstats
+        )
     
-    fn = Path(__file__).parent.parent / 'data' / 'results.csv'
-    results.sort_values('pts', ascending=False).to_csv(fn, index=False)
+        pth = Path(__file__).parent.parent / 'data' / f'player-rater-{catname}.json'
+        with pth.open('w') as f:
+            data = {'data': results.dropna().sort_values('pts', ascending=False).values.tolist()}
+            json.dump(data, f)
 
 
 if __name__ == '__main__':
